dfc6e4d8c8bd45518ed1b651892e4a08d74ca620
Keenan Turley <keenan.turley@gmail.com>
2021-07-14 23:55:26 -0700

    start jamming

--- jam.js
+++ jam.js
@@ -1,23 +1,28 @@
+const jamButton = document.getElementById('jam-button');
 const jam = document.getElementById('jam');
 jam.volume = 0;
 
 const secondsUntilJam = 32.5;
 const timeToPump = 30;
-const maxPump = 0.4;
-const pump = 0.01;
+const maxPump = 0.25;
+const pump = 0.0025;
 
 const startJamming = () => {
+  jamButton.remove();
+  document.body.classList.add('jammer', 'pre-jam');
   setTimeout(
-    () => document.body.classList.add('jamming'), 
+    () => {
+      document.body.classList.remove('pre-jam');
+      document.body.classList.add('jamming');
+    }, 
     secondsUntilJam * 1000
-    );
+  );
     
   jam.play();
   const pumpUpTheJam = setInterval(() => {
-    console.log(jam.volume);
     if (jam.volume >= maxPump) clearInterval(pumpUpTheJam);
     jam.volume += pump;
   }, (1000 * timeToPump) / (maxPump / pump));
 }
 
-jam.addEventListener('canplay', startJamming());
+jamButton.addEventListener('click', startJamming);
